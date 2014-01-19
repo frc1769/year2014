af0078f0893e081cbfe923ab16114cf7ae3f6224
harmonrobotics1769 <harmon.robotics1769@gmail.com>
2014-01-19 17:12:52 -0600

    Partial updates today

--- IterativeTemplate/MyRobot.cpp
+++ IterativeTemplate/MyRobot.cpp
@@ -12,10 +12,13 @@ class RobotDemo : public IterativeRobot
 	CANJaguar rear_left;
 	CANJaguar rear_right;
 	CANJaguar lift_ball_capture;
+	CANJaguar capture_ball;
+	CANJaguar throw_ball;
 	RobotDrive robot_movement; // robot drive system
 	Joystick game_pad; // Game pad joystick
 	Joystick other_stick; // The stick for other control
-	
+	Gyro robot_angle;
+	PIDController control_turn;
 private:
 	double K_p;
 	double K_i;
@@ -24,14 +27,21 @@ private:
 	double y;
 	double turn;
 	double ball_capture_lift_speed;
-
+	bool capture;
+	bool uncapture;
+	bool throw_it;
+	bool un_throw_it;
+	double capture_speed;
+	double throw_speed;
 public:
 	RobotDemo():
 		front_left(3),
 		front_right(14),
 		rear_left(4),
 		rear_right(2),
-		lift_ball_capture(1),
+		lift_ball_capture(5),
+		capture_ball(6),
+		throw_ball(7),
 		robot_movement
 		(
 			front_left, 
@@ -40,8 +50,11 @@ public:
 			rear_right
 		),	
 		game_pad(1),		// as they are declared above.
-		other_stick(2)
+		other_stick(2),
+		robot_angle(1),
+		control_turn(K_p,K_i,K_d,robot_angle,)
 	{
+		robot_angle.SetSensitivity(0.00673);
 		K_p = 0.01;
 		K_i = 0.000009;
 		K_d = 0.0005;
@@ -54,6 +67,8 @@ public:
 		rear_left.ConfigFaultTime(0.2);
 		rear_right.ConfigFaultTime(0.2);
 		robot_movement.SetExpiration(0.2);
+		robot_movement.SetInvertedMotor (RobotDrive::kFrontRightMotor, true);
+		robot_movement.SetInvertedMotor (RobotDrive::kRearRightMotor, true);
 		this->SetPeriod(0); 	//Set update period to sync with robot control packets (20ms nominal)
 	}
 	
@@ -119,11 +134,19 @@ void RobotDemo::TeleopInit() {
  */
 void RobotDemo::TeleopPeriodic() {
 	turn = 0;
+	throw_it = other_stick.GetRawButton(1);
+	un_throw_it = other_stick.GetRawButton(4);
+	capture = other_stick.GetRawButton(2);
+	uncapture = other_stick.GetRawButton(5);
+	throw_speed = throw_it ? 1.0 : (un_throw_it ? -0.4 : 0);
+	capture_speed = capture ? 1.0 : (uncapture ? -1.0 : 0);
 	x = game_pad.GetRawAxis(3); 
 	y = game_pad.GetRawAxis(4);
-	ball_capture_lift_speed = game_pad.GetRawAxis(2);
-	robot_movement.MecanumDrive_Cartesian(x*0.9,y*0.9,turn,0); // drive with arcade style
+	ball_capture_lift_speed = other_stick.GetRawAxis(2);
+	robot_movement.MecanumDrive_Cartesian(x*0.9,y*0.9,turn,0); // drive with mecanum style
 	lift_ball_capture.Set(ball_capture_lift_speed);
+	capture_ball.Set(capture_speed);
+	throw_ball.Set(throw_speed);
 }
 
 /**
